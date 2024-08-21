9b272bf7e5592e5b1df5ad399560281a0916ab66
Erinaldo <erinaldohda@gmail.com>
2024-08-21 20:19:58 -0300

    adicionei comentario

--- biblioteca/biblioteca.sql
+++ biblioteca/biblioteca.sql
@@ -108,7 +108,7 @@ insert into emprestimo (id_livro, aluno, data_emprestimo, data_devolucao, devolv
 -- AQUECIMENTO
 
 -- Q1) Liste todos os livros, mostrando o título, o nome completo do autor (nome e sobrenome) e o nome da editora.
-
+-- select
 -- Q2) Encontre todos os empréstimos que ainda não foram devolvidos. Mostre o título do livro, o nome do aluno (nome e sobrenome) e a data de empréstimo.
 
 -- Q3) Liste todos os livros de um determinado tipo, como "Fantasia". Mostre o título do livro e o nome completo do autor.
